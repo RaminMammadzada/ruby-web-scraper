dfe9b363f7b7c22a37319556060944ac84673b0f
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-10 11:20:06 +0300

    Add kimurai gem instalation into the test configuration file.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,10 +11,6 @@ class Scraper < Kimurai::Base
   @name = 'trendyol_spider'
   @engine = :selenium_chrome
   @start_urls = []
-  # @config = {
-  #     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
-  #     before_request: { delay: 1..3 }
-  # }
 
   @@all_products = []
   @@total_product_count = 0
